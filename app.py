7db92a617847042f2a222c9b1e999b6879f1c8b8
vivek patel <55041874+vivolscute@users.noreply.github.com>
2021-08-31 22:18:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from pywebio.session import *
 
 import numpy as np
 def lr():
-	set_env(title='Linear Regression By Vivek patel!!', output_animation=False)
+	set_env(title='Linear Regression By Vivek patel!!')
 	Nx=input("Please enter how many elements you want to add in X column ",type=NUMBER)
 	Ny=input("Please enter how many elements you want to add in Y column ",type=NUMBER)
 	while(Nx!=Ny):
